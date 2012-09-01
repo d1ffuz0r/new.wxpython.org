bfc495cccb226b42441dda31ae2f99bffc7e24f8
d1ffuz0r <d1fffuz0r@gmail.com>
2012-09-01 18:28:46 +0400

    added simple app

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+
+@app.route('/introduction/')
+def introduction():
+    return render_template('introduction.html')
+
+
+@app.route('/download/')
+def download():
+    return render_template('download.html')
+
+
+@app.route('/quotes/')
+def quotes():
+    return render_template('quotes.html')
+
+@app.route('/screenshots/')
+def screenshots():
+    return render_template('screenshots.html')
+
+@app.route('/recentchanges/')
+def recentchanges():
+    return render_template('recentchanges.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

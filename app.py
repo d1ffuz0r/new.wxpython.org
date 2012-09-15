8ba7c31adaa3a1da1b766a842b16a144971cdcb6
d1ffuz0r <d1fffuz0r@gmail.com>
2012-09-15 15:56:44 +0400

    udpate pages, links

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
 
 @app.route('/introduction/')
